f130c0d6af60e1fc03089822c506015297afc652
Braulio Valdivielso Martínez <brlvldvlsmrtnz@gmail.com>
2015-05-11 19:57:13 +0200

    Fixed bug

--- scripts/closest_neighbour.sh
+++ scripts/closest_neighbour.sh
@@ -6,6 +6,7 @@ DATA_FILE=$1
 bin/closest_neighbour $DATA_FILE > tmp/out.tsp
 
 gnuplot <<EOF
+set size 1, 1
 plot "$DATA_FILE" using 2:3 with points, "tmp/out.tsp" using 2:3 with lines
 pause 100
 EOF
--- src/algorithms/closest_neigbour.cpp
+++ src/algorithms/closest_neigbour.cpp
@@ -10,20 +10,22 @@ vector<City> closestNeighbour(Map problem) {
 	for(int i = 0; i < cities.size() - 1; i++){
 		
 		double minimum_distance = cities[i].distanceTo(cities[i + 1]);
-		int closest_neigbour_index = i + 1;
+		int closest_neighbour_index = i + 1;
 
 		// find the closest neighbour
 		for(int j = i + 1; j < cities.size(); j++){
-			if (cities[i].distanceTo(cities[j]) < minimum_distance){
-				closest_neigbour_index = j;
-				minimum_distance = cities[i].distanceTo(cities[j]);;
+			double current_distance = cities[i].distanceTo(cities[j]);
+
+			if (current_distance < minimum_distance){
+				closest_neighbour_index = j;
+				minimum_distance = current_distance;
 			}
 		}
 
 		// swap next city with closest neighbour
 		City aux = cities[i + 1];
-		cities[i + 1] = cities[closest_neigbour_index];
-		cities[closest_neigbour_index] = aux;
+		cities[i + 1] = cities[closest_neighbour_index];
+		cities[closest_neighbour_index] = aux;
 	}
 
 	cities.push_back(cities[0]); // Return to the first city
